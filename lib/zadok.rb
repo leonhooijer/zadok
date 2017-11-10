50cc43b06f289bc2ba0ebb0c2a3c401891c5ae4a
Leon Hooijer <leonhooijer@outlook.com>
2017-11-10 10:17:10 +0100

    Require filters

--- lib/zadok.rb
+++ lib/zadok.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 require "zadok/engine"
+require "zadok/filter_collection"
 require "zadok/link_renderer"
+
+require "zadok/filters/ransack/base"
+require "zadok/filters/ransack/autocomplete_filter"
+require "zadok/filters/ransack/hidden_filter"
+require "zadok/filters/ransack/multiselect_filter"
+require "zadok/filters/ransack/numeric_range_filter"
+require "zadok/filters/ransack/options_filter"
+require "zadok/filters/ransack/range_filter"
+require "zadok/filters/ransack/select_filter"
+require "zadok/filters/ransack/textfield_filter"
+require "zadok/filters/ransack/typeable_multiselect_filter"
 
 module Zadok
 end
