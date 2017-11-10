536f33bce3670951d67758bfd0229d77993498b8
Leon Hooijer <leonhooijer@outlook.com>
2017-11-10 09:36:17 +0100

    Add stylesheets

--- app/assets/javascripts/zadok.js
+++ app/assets/javascripts/zadok.js
@@ -1,3 +1,8 @@
 //= require jquery3
+//= require rails-ujs
+//= require turbolinks
+//= require jquery.turbolinks
+//= require popper
+//= require bootstrap-sprockets
 
 //= require ./zadok/data_links.js
